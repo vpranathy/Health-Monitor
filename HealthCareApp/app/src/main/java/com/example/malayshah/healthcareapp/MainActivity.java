5b198c062d2e4c3ee1fb2670a980e91b641bb3d0
a-aniket19 <aniket1995anilkumar@gmail.com>
2018-10-27 23:17:46 -0400

    MainActivity deleted

--- HealthCareApp/app/src/main/java/com/example/malayshah/healthcareapp/MainActivity.java
+++ HealthCareApp/app/src/main/java/com/example/malayshah/healthcareapp/MainActivity.java
@@ -3,19 +3,19 @@ package com.example.malayshah.healthcareapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button buttonA;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonA = (Button) findViewById(R.id.buttonA);
+        Button buttonA = (Button) findViewById(R.id.buttonA);
         buttonA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
