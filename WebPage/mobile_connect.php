81904398153aaa2574411b734de8e83882e5fa48
yuyangchen0122 <chenyuyang0122@gmail.com>
2018-10-30 14:43:42 -0400

    update table.php

--- WebPage/mobile_connect.php
+++ WebPage/mobile_connect.php
@@ -1,12 +1,14 @@
 <?php
 
-$host = "softenggroup2.czmkb4udcq6o.us-east-2.rds.amazonaws.com"
-$db_user = "yuyangchen0122"
-$db_password="a123123q45"
-$db_name = "HealthMonitoring"
+$host = 'softenggroup2.czmkb4udcq6o.us-east-2.rds.amazonaws.com';
+$db_user = 'yuyangchen0122';
+$db_password='a123123q45';
+$db_name = 'HealthMonitoring';
 
 
 $con = mysqli_connect($host, $db_user, $db_password, $db_name);
 
-
+if($con){
+	echo "Success";
+}
 ?>
