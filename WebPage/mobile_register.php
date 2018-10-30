f627ff6e49611e88239b603fec37c23a56ece820
a-aniket19 <aniket1995anilkumar@gmail.com>
2018-10-30 11:52:22 -0400

    changed mobile_register.php

--- WebPage/mobile_register.php
+++ WebPage/mobile_register.php
@@ -1,5 +1,7 @@
 <?php
 require "mobile_connect.php";
+
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $user_name = $_POST["user_name"];
@@ -9,7 +11,7 @@ $password =$_POST["password"];
 $sql =" Select * from users where email like '".$email."';";
 $result= mysqli_query($con,$sql);
 $response = array();
- (mysqli_num_rows($result)>0) {
+ if (mysqli_num_rows($result)>0) {
 	$code = " Registration failed";
 	$message = " User already exists....";
 	array_push($response,array('code' =>$code,'message'=>$message));
