5154d02afd9af03370f0f6115340b00dfc27eb9d
a-aniket19 <aniket1995anilkumar@gmail.com>
2018-10-31 02:02:09 -0400

    adding webpage.zip and changed mobile_heartrate.php

--- WebPage/mobile_heartrate.php
+++ WebPage/mobile_heartrate.php
@@ -6,10 +6,10 @@ $username = $_POST["username"];
 $heartrate = $_POST["heartrate"];
 $date = $_POST["date"];
 $time =$_POST["time"];
-$music = $_POST["music"]
+$music = $_POST["music"];
 
-$response = array();
 $sql= " insert into HeartData (username,HeartRate, Date, Time , Music) Values('".$username."','".$heartrate."','".$date."','".$time."','".$music."');";
+$response = array();
 $result= mysqli_query($con,$sql);
 $code = " Registered Successfully";
 array_push($response,array('code' =>$code));
